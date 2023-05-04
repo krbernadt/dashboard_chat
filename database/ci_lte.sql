6cad22e8fefe39eedb83438c269cb5afc9ab110f
Bernard Tampubolon <102019218+krbernadt@users.noreply.github.com>
2023-05-04 13:26:19 +0700

    Update ci_lte.sql

--- database/ci_lte.sql
+++ database/ci_lte.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 04, 2023 at 08:23 AM
+-- Generation Time: May 04, 2023 at 08:26 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
